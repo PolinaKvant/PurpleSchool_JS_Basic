61e0606f797f719b74046a557aa6f67125b8c6e0
Polina <polinal1883@yandex.ru>
2023-08-05 19:09:10 +0300

    add_folder_classwork

---
+++ classwork/number_2.js
@@ -0,0 +1,10 @@
+const money = 100;
+if (money>50){
+    console.log('монет больше 50');
+}
+else if (money>5){
+    console.log('монет больше 5, но меньше 50');
+}
+else {
+    console.log('монет меньше 5');
+}
