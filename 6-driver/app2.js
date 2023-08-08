9a4e297aa0836627860ca7ea57413a5f1279afb9
Polina <polinal1883@yandex.ru>
2023-08-08 12:49:20 +0300

    making_edits_2

--- 6-driver/app2.js
+++ 6-driver/app2.js
@@ -1,6 +1,6 @@
-const hasLicence = 'yes';
+const hasLicence = true;
 const age = 18;
 const isDrunk = false;
 
-const answ = hasLicence==='yes' && age>=18 && isDrunk===false;
+const answ = hasLicence===true && age>=18 && isDrunk===false;
 console.log(answ ? 'может' : 'не может');
