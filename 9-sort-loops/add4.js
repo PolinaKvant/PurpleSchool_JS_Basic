3350aa9fa04b15a139219db30caa236534b659fa
Polina <polinal1883@yandex.ru>
2023-11-12 20:41:33 +0300

    commit_four

--- 9-sort-loops/add4.js
+++ 9-sort-loops/add4.js
@@ -1,12 +1,16 @@
-const arr = [1, 40, -5, 10, 0];
-function sort (arr){
-    for (let i=0; i<arr.length;i++){
-        for (let j=0; j<arr.length;j++){
-            if (arr[j]>arr[j+1] && j!=arr.length-1){
-                [arr[j],arr[j+1]]=[arr[j+1],arr[j]];
-            }
+
+
+function sortArray(arr) {
+    let len = arr.length;
+    for (let i = 0; i < len; i++) {
+      for (let j = 0; j < len - i - 1; j++) {
+        if (arr[j] > arr[j + 1]) {
+            [arr[j],arr[j+1]]=[arr[j+1],arr[j]];
         }
+      }
     }
     return arr;
-}
-console.log(sort(arr));
+  }
+  
+  let arr = [1, 40, -5, 10, 0];
+  console.log(sortArray(arr)); 
