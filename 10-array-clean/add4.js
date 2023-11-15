248fdcd468d53b1f77fec363bf0fd260f0d89a6d
Polina <polinal1883@yandex.ru>
2023-11-15 23:04:13 +0300

    commit two

--- 10-array-clean/add4.js
+++ 10-array-clean/add4.js
@@ -10,9 +10,9 @@ function filterArray(arr, removeFunc) {
   }
   
   // Исходный массив
-  var numbers = [1, 3, 5, 7, 9, 2, 4, 6, 8, 10];
+  let numbers = [1, 3, 5, 7, 9, 2, 4, 6, 8, 10];
   
   // Отфильтрованный массив
-  var filteredArray = filterArray(numbers, removeNum);
+  let filteredArray = filterArray(numbers, removeNum);
   
   console.log(filteredArray); // [7, 9, 6, 8, 10]
--- 11-date-array/add4.js
+++ 11-date-array/add4.js
@@ -1,18 +1,14 @@
-function filterValidDates(arr) {
-  var validDates = [];
-  
-  for (var i = 0; i < arr.length; i++) {
-    var date = new Date(arr[i]);
-    
-    if (!isNaN(date.getTime())) {
-      validDates.push(arr[i]);
+
+const array = ['10/02/2022', 'тест', '11/12/2023', '00/13/2022', '41/12/2023'];
+for(let i = 0; i < array.length; i++){
+  //оставляем только цифры
+  const date = array[i].replace(/[^0-9 ]/g,'');
+  //проверка на правильность даты
+  if (date % 10000 < 2024){
+    if (date / 10000 % 100 < 13 && date / 10000 % 100 > 0){
+      if (date / 1000000 < 32){
+        console.log(`${parseInt(date / 1000000)}-${parseInt(date / 10000) % 100}-${date % 10000}`);
+      }
     }
   }
-  
-  return validDates;
 }
-
-var array = ['10/02/2022', 'тест', '11/12/2023', '00/13/2022', '41/12/2023'];
-var filteredArray = filterValidDates(array);
-
-console.log(filteredArray);
